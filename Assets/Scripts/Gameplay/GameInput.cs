20a82db677de43732fcabaf679bf580d2026ea10
Murat Öz <ozmurat@windowslive.com>
2023-04-26 20:58:43 +0300

    Added rebinding in options

--- Assets/Scripts/Gameplay/GameInput.cs
+++ Assets/Scripts/Gameplay/GameInput.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using Utility;
 
 namespace Gameplay
 {
-	public class GameInput : MonoBehaviour
+	public class GameInput : Singleton<GameInput>
 	{
+		public enum Binding
+		{
+			MoveUp,
+			MoveDown,
+			MoveLeft,
+			MoveRight,
+			Interact,
+			InteractAlt,
+			Pause
+		}
+
 		private PlayerInputActions playerInputActions;
+
+		private const string BINDINGS_PLAYERPREFS = "InputBindings";
 
 		public static event EventHandler OnInteractAction;
 		public static event EventHandler OnInteractAltAction;
@@ -15,6 +30,8 @@ namespace Gameplay
 		private void Awake()
 		{
 			playerInputActions = new PlayerInputActions();
+			if (PlayerPrefs.HasKey(BINDINGS_PLAYERPREFS))
+				playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(BINDINGS_PLAYERPREFS));
 			playerInputActions.Player.Enable();
 
 			playerInputActions.Player.Interact.performed += OnInteract;
@@ -51,6 +68,73 @@ namespace Gameplay
 		private void OnInteractAlt(InputAction.CallbackContext context)
 		{
 			OnInteractAltAction?.Invoke(this, EventArgs.Empty);
+		}
+
+		public string GetBindingText(Binding binding)
+		{
+			return binding switch
+			{
+				Binding.MoveUp => playerInputActions.Player.Move.bindings[1].ToDisplayString(),
+				Binding.MoveDown => playerInputActions.Player.Move.bindings[2].ToDisplayString(),
+				Binding.MoveLeft => playerInputActions.Player.Move.bindings[3].ToDisplayString(),
+				Binding.MoveRight => playerInputActions.Player.Move.bindings[4].ToDisplayString(),
+				Binding.Interact => playerInputActions.Player.Interact.bindings[0].ToDisplayString(),
+				Binding.InteractAlt => playerInputActions.Player.InteractAlt.bindings[0].ToDisplayString(),
+				Binding.Pause => playerInputActions.Player.Pause.bindings[0].ToDisplayString(),
+				_ => throw new ArgumentOutOfRangeException(nameof(binding), binding, null)
+			};
+		}
+
+		public void Rebind(Binding binding, UnityAction onRebound)
+		{
+			playerInputActions.Player.Disable();
+			InputAction inputAction;
+			int bindingIndex;
+
+			switch (binding)
+			{
+				case Binding.MoveUp:
+					inputAction = playerInputActions.Player.Move;
+					bindingIndex = 1;
+					break;
+				case Binding.MoveDown:
+					inputAction = playerInputActions.Player.Move;
+					bindingIndex = 2;
+					break;
+				case Binding.MoveLeft:
+					inputAction = playerInputActions.Player.Move;
+					bindingIndex = 3;
+					break;
+				case Binding.MoveRight:
+					inputAction = playerInputActions.Player.Move;
+					bindingIndex = 4;
+					break;
+				case Binding.Interact:
+					inputAction = playerInputActions.Player.Interact;
+					bindingIndex = 0;
+					break;
+				case Binding.InteractAlt:
+					inputAction = playerInputActions.Player.InteractAlt;
+					bindingIndex = 0;
+					break;
+				case Binding.Pause:
+					inputAction = playerInputActions.Player.Pause;
+					bindingIndex = 0;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(binding), binding, null);
+			}
+
+			inputAction.PerformInteractiveRebinding(bindingIndex).OnComplete(callback =>
+			{
+				callback.Dispose();
+				playerInputActions.Player.Enable();
+
+				onRebound?.Invoke();
+
+				PlayerPrefs.SetString(BINDINGS_PLAYERPREFS, playerInputActions.SaveBindingOverridesAsJson());
+				PlayerPrefs.Save();
+			}).Start();
 		}
 	}
 }
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -1,15 +1,40 @@
-﻿using Managers;
+﻿using Gameplay;
+using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Utility;
 
 namespace UI
 {
-	public class OptionsUI : Singleton<OptionsUI>
+	public class OptionsUI : Utility.Singleton<OptionsUI>
 	{
+		[SerializeField] private Button closeButton;
+
+		[Header("Audio")]
 		[SerializeField] private Slider soundEffectSlider;
 		[SerializeField] private Slider musicSlider;
-		[SerializeField] private Button closeButton;
+
+		[Header("Controls")]
+		[SerializeField] private Button btnMoveUp;
+		[SerializeField] private Button btnMoveDown;
+		[SerializeField] private Button btnMoveLeft;
+		[SerializeField] private Button btnMoveRight;
+		[SerializeField] private Button btnInteract;
+		[SerializeField] private Button btnInteractAlt;
+		[SerializeField] private Button btnPause;
+		private TMP_Text txtMoveUp;
+		private TMP_Text txtMoveDown;
+		private TMP_Text txtMoveLeft;
+		private TMP_Text txtMoveRight;
+		private TMP_Text txtInteract;
+		private TMP_Text txtInteractAlt;
+		private TMP_Text txtPause;
+		[SerializeField] private GameObject pressToRebindPanel;
+
+		private void Awake()
+		{
+			InitControlButtons();
+		}
 
 		private void Start()
 		{
@@ -17,15 +42,38 @@ namespace UI
 			musicSlider.onValueChanged.AddListener(MusicChanged);
 			closeButton.onClick.AddListener(Hide);
 
+			btnMoveUp.onClick.AddListener(() => RebindBinding(GameInput.Binding.MoveUp));
+			btnMoveDown.onClick.AddListener(() => RebindBinding(GameInput.Binding.MoveDown));
+			btnMoveLeft.onClick.AddListener(() => RebindBinding(GameInput.Binding.MoveLeft));
+			btnMoveRight.onClick.AddListener(() => RebindBinding(GameInput.Binding.MoveRight));
+			btnInteract.onClick.AddListener(() => RebindBinding(GameInput.Binding.Interact));
+			btnInteractAlt.onClick.AddListener(() => RebindBinding(GameInput.Binding.InteractAlt));
+			btnPause.onClick.AddListener(() => RebindBinding(GameInput.Binding.Pause));
+
 			soundEffectSlider.value = SoundManager.Instance.Volume * 10;
 			musicSlider.value = MusicManager.Instance.Volume * 10;
+
 			GameManager.Instance.OnGameUnpaused += OnGameUnpaused;
+
+			UpdateVisuals();
+			HidePressToRebindPanel();
 			Hide();
 		}
 
 		private void OnGameUnpaused()
 		{
 			Hide();
+		}
+
+		private void UpdateVisuals()
+		{
+			txtMoveUp.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.MoveUp));
+			txtMoveDown.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.MoveDown));
+			txtMoveLeft.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.MoveLeft));
+			txtMoveRight.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.MoveRight));
+			txtInteract.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Interact));
+			txtInteractAlt.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlt));
+			txtPause.SetText(GameInput.Instance.GetBindingText(GameInput.Binding.Pause));
 		}
 
 		private void SoundEffectChanged(float value)
@@ -47,6 +95,37 @@ namespace UI
 		{
 			PlayerPrefs.Save();
 			gameObject.SetActive(false);
+		}
+
+		private void ShowPressToRebindPanel()
+		{
+			pressToRebindPanel.SetActive(true);
+		}
+
+		private void HidePressToRebindPanel()
+		{
+			pressToRebindPanel.SetActive(false);
+		}
+
+		private void RebindBinding(GameInput.Binding binding)
+		{
+			ShowPressToRebindPanel();
+			GameInput.Instance.Rebind(binding, () =>
+			{
+				HidePressToRebindPanel();
+				UpdateVisuals();
+			});
+		}
+
+		private void InitControlButtons()
+		{
+			txtMoveUp = btnMoveUp.GetComponentInChildren<TMP_Text>();
+			txtMoveDown = btnMoveDown.GetComponentInChildren<TMP_Text>();
+			txtMoveLeft = btnMoveLeft.GetComponentInChildren<TMP_Text>();
+			txtMoveRight = btnMoveRight.GetComponentInChildren<TMP_Text>();
+			txtInteract = btnInteract.GetComponentInChildren<TMP_Text>();
+			txtInteractAlt = btnInteractAlt.GetComponentInChildren<TMP_Text>();
+			txtPause = btnPause.GetComponentInChildren<TMP_Text>();
 		}
 	}
 }
