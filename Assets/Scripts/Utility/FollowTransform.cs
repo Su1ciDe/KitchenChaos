fba232e42f059b78e1114f73c7ed0a498fd32346
Murat Öz <ozmurat@windowslive.com>
2023-04-30 21:02:21 +0300

    Fixed object pickup/drop

--- Assets/Scripts/Utility/FollowTransform.cs
+++ Assets/Scripts/Utility/FollowTransform.cs
@@ -3,11 +3,13 @@ using UnityEngine.Animations;
 
 namespace Utility
 {
+	[RequireComponent(typeof(PositionConstraint))]
 	public class FollowTransform : MonoBehaviour
 	{
 		private Transform targetTransform;
 
 		private PositionConstraint positionConstraint;
+		private ConstraintSource source;
 
 		private void Awake()
 		{
@@ -16,9 +18,11 @@ namespace Utility
 
 		public void SetTargetTransform(Transform target)
 		{
+			if (positionConstraint.sourceCount > 0)
+				positionConstraint.RemoveSource(0);
 			targetTransform = target;
 
-			var source = new ConstraintSource { sourceTransform = targetTransform, weight = 1 };
+			source = new ConstraintSource { sourceTransform = targetTransform, weight = 1 };
 			positionConstraint.AddSource(source);
 			positionConstraint.constraintActive = true;
 		}
