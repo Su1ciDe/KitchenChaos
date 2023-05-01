d62a310f7068bb479203b6b725f5b533fac50804
Murat Öz <ozmurat@windowslive.com>
2023-05-01 16:40:26 +0300

    Synced pause

---
+++ Assets/Scripts/UI/GamePauseMultiplayerUI.cs
@@ -0,0 +1,24 @@
+﻿using Managers;
+
+namespace UI
+{
+	public class GamePauseMultiplayerUI : BaseUI
+	{
+		private void Start()
+		{
+			GameManager.Instance.OnMultiplayerGamePaused += OnMultiplayerGamePaused;
+			GameManager.Instance.OnMultiplayerGameUnpaused += OnMultiplayerGameUnpaused;
+			Hide();
+		}
+
+		private void OnMultiplayerGameUnpaused()
+		{
+			Hide();
+		}
+
+		private void OnMultiplayerGamePaused()
+		{
+			Show();
+		}
+	}
+}
